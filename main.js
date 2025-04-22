2137fc31dc15600be114d70389d3467ab5285d57
sily-cat <joshytfan@gmail.com>
2025-04-22 12:53:17 -0700

    bugfixing (surprised?)

--- main.js
+++ main.js
@@ -282,7 +282,8 @@ Deno.serve(async (req) => {
                         output_octad.splice(12, 0, " ");
                         output_octad.splice(8, 0, " ");
                         output_octad.splice(4, 0, " ");
-                        if(body.data.options[0].options.length > 1) {
+                        console.log(body.data.options[0].options);
+                        if(body.data.options[0].options[1] != undefined) {
                             if (body.data.options[0].options[1] == false) {
                                 payload.data.content = output_octad.join("");
                                 break;
